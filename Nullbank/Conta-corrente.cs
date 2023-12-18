379f25bb277462ea125277e69ab2a590882607df
Carlos Eduardo <Carlos Eduardo@DESKTOP-BVFKLND>
2023-12-18 14:21:25 -0300

    metodos saca/deposita/transferir de cada conta

--- Nullbank/Conta-corrente.cs
+++ Nullbank/Conta-corrente.cs
@@ -18,5 +18,51 @@ namespace Nullbank
         {
             Console.WriteLine($"Saldo da conta corrente {NumeroConta} de {Usuario}: R${Saldo}, Limite de Crédito: R${limitecredito}");
         }
+        public override void Sacar(double valor)
+        {
+            if (valor > 0)
+            {
+                double valorTotal = Saldo + limitecredito;
+                if (valorTotal >= valor)
+                {
+                    Saldo -= valor;
+                    string transacao = $"Saque de R${valor} (conta corrente)";
+                    historico.Add(transacao);
+                    Console.WriteLine(transacao);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo e limite de crédito insuficientes para realizar o saque.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O valor do saque deve ser maior que zero.");
+            }
+        }
+        public override void Transferir(Conta contaDestino, double valor)
+        {
+            if (valor > 0)
+            {
+                double valorTotal = Saldo + limitecredito;
+                if (valorTotal >= valor)
+                {
+                    Saldo -= valor;
+                    contaDestino.Saldo += valor;
+
+                    string transacao = $"Transferência de R${valor} para a conta {contaDestino.NumeroConta} (conta corrente)";
+                    historico.Add(transacao);
+                    Console.WriteLine(transacao);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo e limite de crédito insuficientes para realizar a transferência.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O valor da transferência deve ser maior que zero.");
+            }
+        }
     }
 }
--- Nullbank/Conta-poupanca.cs
+++ Nullbank/Conta-poupanca.cs
@@ -10,7 +10,7 @@ namespace Nullbank
     {
         public double TaxaRendimento { get; }
 
-        public Conta_poupanca(int NumeroConta, string Usuario, double saldoInicial, double TaxaRendimento): base(NumeroConta, Usuario, saldoInicial)
+        public Conta_poupanca(int NumeroConta, string Usuario, double saldoInicial, double TaxaRendimento) : base(NumeroConta, Usuario, saldoInicial)
         {
             this.TaxaRendimento = TaxaRendimento;
         }
@@ -19,6 +19,49 @@ namespace Nullbank
         {
             Console.WriteLine($"Saldo da conta poupança {NumeroConta} de {Usuario}: R${Saldo}, Taxa de Rendimento: {TaxaRendimento}%");
         }
-            
+        public override void Sacar(double valor)
+        {
+            if (valor > 0)
+            {
+                if (Saldo >= valor)
+                {
+                    Saldo -= valor;
+                    string transacao = $"Saque de R${valor} (conta poupança)";
+                    historico.Add(transacao);
+                    Console.WriteLine(transacao);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente para realizar o saque.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O valor do saque deve ser maior que zero.");
+            }
+        }
+        public override void Transferir(Conta contaDestino, double valor)
+        {
+            if (valor > 0)
+            {
+                if (Saldo >= valor)
+                {
+                    Saldo -= valor;
+                    contaDestino.Saldo += valor;
+
+                    string transacao = $"Transferência de R${valor} para a conta {contaDestino.NumeroConta} (conta poupança)";
+                    historico.Add(transacao);
+                    Console.WriteLine(transacao);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente para realizar a transferência.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O valor da transferência deve ser maior que zero.");
+            }
+        }
     }
 }
