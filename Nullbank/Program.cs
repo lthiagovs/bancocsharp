05dbfca33ae915afb1964173fabea908732f3dcc
lthiagovs <lthiagovs@gmail.com>
2023-12-18 17:47:40 -0300

    Interface do Gerente

--- Nullbank/Program.cs
+++ Nullbank/Program.cs
@@ -2,13 +2,14 @@
 using Nullbank.Arquivos;
 using Nullbank.Usuarios;
 class Program
-{ 
+{
     public static void Main()
     {
 
         bool rodar = true;
         bool logado = false;
         Usuario usuario = null;
+        int tipo = 0;
         string escolha;
 
         //Main Loop
@@ -20,23 +21,113 @@ class Program
             if (logado)
             {
                 Console.WriteLine("Logado como: " + usuario.nome);
-                switch(usuario)
+                switch (tipo)
                 {
                     //Opções do Gerente
-                    case Gerente: 
+                    case 3:
+                        Console.WriteLine("0 - Sair\n1 - Criar Funcionario\n2 - Buscar Funcionario\n3 - Editar Funcionario");
+                        escolha = Console.ReadLine();
+
+                        switch (int.Parse(escolha))
+                        {
+
+                            //LogOff
+                            case 0:
+                                logado = false;
+                                break;
+
+                            //Criar Funcionario
+                            case 1:
+                                Console.Write("CPF >> ");
+                                string cpf = Console.ReadLine();
+
+                                Console.Write("NOME >> ");
+                                string nome = Console.ReadLine();
+
+                                Console.Write("CEP >> ");
+                                string cep = Console.ReadLine();
+
+                                Console.Write("NUMERO >> ");
+                                int num = int.Parse(Console.ReadLine());
+
+                                Console.Write("RUA >> ");
+                                string rua = Console.ReadLine();
+
+                                Console.Write("CIDADE >> ");
+                                string cidade = Console.ReadLine();
+
+                                Console.Write("ESTADO >> ");
+                                string estado = Console.ReadLine();
+
+                                Console.Write("AGENCIA >> ");
+                                int agencia = int.Parse(Console.ReadLine());
+
+                                Console.Write("SENHA >> ");
+                                string senha = Console.ReadLine();
+
+                                Endereço funcionarioEndereço = new Endereço(cep, num, rua, cidade, estado);
+                                Funcionario nFuncionario = new Funcionario(nome, cpf, funcionarioEndereço, agencia, senha);
+
+                                Arquivo.criaArquivoUsuario(nFuncionario);
+                                break;
+
+                            //Buscar Funcionario
+                            case 2 or 3:
+
+                                Console.Write("NOME >> ");
+                                string nomeFuncionario = Console.ReadLine();
+
+                                Usuario funcBusca = new Funcionario(nomeFuncionario, "", null, 1, "");
+                                funcBusca = Arquivo.buscaUsuario(funcBusca);
+
+                                if (!funcBusca.cpf.Equals(""))
+                                {
+                                    Console.WriteLine("\nFuncionario encontrado: ");
+                                    Console.WriteLine("Nome: " + funcBusca.nome);
+                                    Console.WriteLine("CPF:" + funcBusca.cpf);
+                                    Console.WriteLine("Agencia: " + funcBusca.agencia);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nenhum funcionario com este nome foi encontrado!");
+                                }
+
+                                if (int.Parse(escolha) == 3)
+                                {
+                                    Console.WriteLine("\nAlterando Funcionario: ");
+
+                                    Console.Write("CPF >> ");
+                                    string cpfFunc = Console.ReadLine();
+
+                                    Console.Write("AGENCIA >> ");
+                                    int agenciaFunc = int.Parse(Console.ReadLine());
+
+                                    Console.WriteLine("\nFuncionario alterado! ");
+
+                                }
+
+                                Console.ReadLine();
+
+                                break;
+
+                        }
+
                         break;
 
                     //Opções do Cliente
-                    case Cliente: break;
+                    case 1:
+                        Console.WriteLine("Cliente");
+                        escolha = Console.ReadLine();
+                        break;
 
                     //Opções do Funcionario
-                    case Funcionario:
+                    case 2:
                         Console.WriteLine("0 - sair\n1 - 1 criar Cliente\n2 - 2 Buscar cliente\n3 - 3 Editar cliente");
                         escolha = Console.ReadLine();
-                        switch (int.Parse(escolha)) 
+                        switch (int.Parse(escolha))
                         {
                             case 0:
-                                logado=false ;
+                                logado = false;
                                 break;
                             case 1:
                                 Console.WriteLine("Criando novo cliente...");
@@ -70,15 +161,15 @@ class Program
 
                                 Endereço clienteEndereço = new Endereço(cep, num, rua, cidade, estado);
 
-                                Cliente novoCliente = new Cliente( cpf1, nome, clienteEndereço ,agencia,senha);
+                                Cliente novoCliente = new Cliente(cpf1, nome, clienteEndereço, agencia, senha);
 
                                 Arquivo.criaArquivoUsuario(novoCliente);
-                                break; 
+                                break;
                             case 2:
                                 Console.WriteLine("Buscando cliente...");
                                 Console.Write("Nome do Cliente: ");
                                 string nome1 = Console.ReadLine();
-                                Usuario clienteEncontrado = new Cliente(nome1, "",null,0,"");
+                                Usuario clienteEncontrado = new Cliente(nome1, "", null, 0, "");
                                 clienteEncontrado = Arquivo.buscaUsuario(clienteEncontrado);
                                 if (clienteEncontrado.cpf.Equals(""))
                                 {
@@ -90,24 +181,27 @@ class Program
                                 }
                                 break;
                             case 3:
-                                 Console.WriteLine("Editando cliente...");
-                                 Console.Write("Nome do Cliente: ");
-                                 string nome2 = Console.ReadLine();
-                                 Usuario clienteParaEditar = new Cliente(nome2, "", null, 0, "");
-                                 clienteParaEditar = Arquivo.buscaUsuario(clienteParaEditar);
-                                 if (!clienteParaEditar.cpf.Equals(""))
-                                 {
-                                   //vazio á fazer
-                                 }
-                                 else
-                                 {
-                                 Console.WriteLine("Cliente não encontrado. Não é possível editar.");
-                                 }
-                                 break;
-                            default: Console.WriteLine("opção invalida");
+                                Console.WriteLine("Editando cliente...");
+                                Console.Write("Nome do Cliente: ");
+                                string nome2 = Console.ReadLine();
+                                Usuario clienteParaEditar = new Cliente(nome2, "", null, 0, "");
+                                clienteParaEditar = Arquivo.buscaUsuario(clienteParaEditar);
+                                if (!clienteParaEditar.cpf.Equals(""))
+                                {
+                                    //vazio á fazer
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Cliente não encontrado. Não é possível editar.");
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("opção invalida");
                                 break;
                         }
-                    break;
+                        break;
+                    default:
+                        break;
                 }
             }
             //Usuario não está logado
@@ -117,34 +211,42 @@ class Program
                 Console.WriteLine("Realize o login:\n0 - Sair\n1 - Clientes\n2 - Funcionarios\n3 - Gerentes");
                 Console.Write("Opção >> ");
                 escolha = Console.ReadLine();
-                Console.Write("Login >> ");
-                string login = Console.ReadLine();
-                Console.Write("Senha >> ");
-                string senha = Console.ReadLine();
-                Usuario user;
-
-                //Instanciando usuario vazio para iniciar busca / baseado no tipo
-                switch (int.Parse(escolha))
+                if (int.Parse(escolha) != 0)
                 {
-                    case 2:
-                        user = new Funcionario(login, "", new Endereço("", 0, "", "", ""), 0, ""); 
-                        break;
-                    case 3:
-                        //Console.Write("GERENTE");
-                        user = new Gerente(login, "", new Endereço("", 0, "", "", ""), 0, "");
-                        break;
-                    default:
-                        user = new Cliente(login, "", new Endereço("", 0, "", "", ""), 0, "");
-                        break;
+                    Console.Write("Login >> ");
+                    string login = Console.ReadLine();
+                    Console.Write("Senha >> ");
+                    string senha = Console.ReadLine();
+                    Usuario user = new Cliente(login, "", new Endereço("", 0, "", "", ""), 0, "");
 
+                    //Instanciando usuario vazio para iniciar busca / baseado no tipo
+                    switch (int.Parse(escolha))
+                    {
+                        case 2:
+                            user = new Funcionario(login, "", new Endereço("", 0, "", "", ""), 0, "");
+                            tipo = 2;
+                            break;
+                        case 3:
+                            user = new Gerente(login, "", new Endereço("", 0, "", "", ""), 0, "");
+                            tipo = 3;
+                            break;
+                        default:
+                            tipo = 1;
+                            break;
+
+                    }
+
+                    //Verificar Login
+                    user = Arquivo.buscaUsuario(user);
+                    if (!user.cpf.Equals(""))
+                    {
+                        usuario = user;
+                        logado = true;
+                    }
                 }
-
-                //Verificar Login
-                user = Arquivo.buscaUsuario(user);
-                if (!user.cpf.Equals(""))
+                else
                 {
-                    usuario = user;
-                    logado = true;
+                    rodar = false;
                 }
 
             }
@@ -155,4 +257,3 @@ class Program
     }
 
 }
-
