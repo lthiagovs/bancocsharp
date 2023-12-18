92ddbddd442b82ab11f16bb00fdd3df04024de01
Carlos Eduardo <Carlos Eduardo@DESKTOP-BVFKLND>
2023-12-18 10:26:19 -0300

    contas  poupança, corrente, compartilhada

---
+++ Nullbank/Conta-compartilhada.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullbank
+{
+    internal class Conta_compartilhada : Conta
+    {
+        public List<string> UsuariosSecundarios { get; }
+
+        public Conta_compartilhada(int NumeroConta, string Usuario, double saldoInicial, List<string> UsuariosSecundarios) : base(NumeroConta, Usuario, saldoInicial)
+        {
+            this.UsuariosSecundarios = UsuariosSecundarios;
+        }
+    }
+}
---
+++ Nullbank/Conta-poupanca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullbank
+{
+    internal class Conta_poupanca : Conta
+    {
+        public double TaxaRendimento { get; }
+
+        public Conta_poupanca(int NumeroConta, string Usuario, double saldoInicial, double TaxaRendimento): base(NumeroConta, Usuario, saldoInicial)
+        {
+            this.TaxaRendimento = TaxaRendimento;
+        }
+
+        public new void ConsultarSaldo()
+        {
+            Console.WriteLine($"Saldo da conta poupança {NumeroConta} de {Usuario}: R${Saldo}, Taxa de Rendimento: {TaxaRendimento}%");
+        }
+            
+    }
+}
