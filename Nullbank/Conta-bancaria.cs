34321d07a150515884973c256b536b2bda470558
Carlos Eduardo <Carlos Eduardo@DESKTOP-BVFKLND>
2023-12-18 08:44:59 -0300

    fiz o começo da classe conta

---
+++ Nullbank/Conta-bancaria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullbank
+{
+    abstract class Conta
+    {
+        public int NumeroConta;
+        public string Titular;
+        public double Saldo;
+        public List<string> historico;
+        public int idade;
+
+        public ContaBancaria(int numeroConta, string titular, double saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+            Saldo = saldoInicial;
+            historico = new List<string>();
+
+        }
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Saldo da conta {NumeroConta} de {Titular}: {Saldo} reais");
+        }
+
+
+
+
+
+
+
+    }
+}
